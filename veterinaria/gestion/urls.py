cbf9d4a2d55a7032b1f689d0ed9ade2b012d867e
carmenuv <carmenurizarv@gmail.com>
2022-12-03 20:45:58 -0500

    Agregué requirements.txt

---
+++ veterinaria/gestion/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import EspecieApiView
+
+urlpatterns = [
+    # el metodo as_view convierte la clase en una vista para que pueda ser consumida por Django
+    path('especie/', EspecieApiView.as_view()),
+]
