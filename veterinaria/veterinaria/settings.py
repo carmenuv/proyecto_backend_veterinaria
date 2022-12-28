b0a11d38fed0f4929d899713595991c4faf52727
vilrock <67030207+vilrock@users.noreply.github.com>
2022-12-28 08:55:25 -0500

    subiendo repositorio

--- veterinaria/veterinaria/settings.py
+++ veterinaria/veterinaria/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,7 +128,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3080"
+]
 
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3080"
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
